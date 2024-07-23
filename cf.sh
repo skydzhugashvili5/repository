f9e0ef3092aefdb3e9c045d1148800ba488e3647
skydzhugashvili5 <nicholas42tu.rne.r1.1.97@gmail.com>
2024-07-23 14:42:49 +0800

    Create cf.sh

---
+++ cf.sh
@@ -0,0 +1,5 @@
+curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
+
+sudo dpkg -i cloudflared.deb && 
+
+sudo cloudflared service install eyJhIjoiNzQ4MmYzNTIwMTU2OTg0MTA3MTBiYjA2NjljMWQzMWYiLCJ0IjoiNjQ3NTJmMmEtMDg2MC00ZmIwLTkxZGItY2NiNTU4MDRkMjczIiwicyI6IlpEWXlPV1U1TlRFdFlqZ3hOQzAwWVRVMUxUaGtNekV0T0dSaVl6a3dNMlpoT1dGayJ9
